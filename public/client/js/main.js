ee4dd6aeb01514f419de17b531660450585a7764
Auvergne <auvergnerw@gmail.com>
2014-06-21 15:25:09 +0800

    sandbox handlebars api calls

--- public/client/js/main.js
+++ public/client/js/main.js
@@ -1,7 +1,8 @@
 require.config({
     baseUrl: '',
     paths: {
-        'handlebars': 'bower_components/handlebars/handlebars'
+        'handlebars': 'bower_components/handlebars/handlebars',
+        'jquery': 'bower_components/jquery/dist/jquery.min'
     },
     shim: {
         'handlebars': {
@@ -10,8 +11,15 @@ require.config({
     }
 });
 
-require([ 'handlebars'
-], function (Handlebars) {
+require([ 'handlebars', 'jquery'
+], function (Handlebars, $) {
     "use strict";
+
     console.log("hello world");
+
+    var test_template_raw = "<p>Hello {{ name }} </p>";
+    var compiled_template = Handlebars.compile(test_template_raw);
+    var rendered_template = compiled_template({name: "bob"});
+    $('#hello-test').html(rendered_template);
+
 });
