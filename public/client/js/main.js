eee6ccd5a6a001a402593ede7572a6a3722c5c44
Auvergne <auvergnerw@gmail.com>
2014-06-22 04:10:40 +0800

    reimplement sample routes using window.setInterval for "events"

--- public/client/js/main.js
+++ public/client/js/main.js
@@ -7,6 +7,7 @@ require.config({
         'backbone': 'bower_components/backbone/backbone',
         'lodash': 'bower_components/lodash/dist/lodash.min',
         'when': 'bower_components/when',
+        'domReady': 'bower_components/requirejs-domready/domReady',
         'text': 'vendor/text',
 
         'models': 'js/models'
@@ -28,7 +29,8 @@ require.config({
 require([ 'handlebars', 'jquery', 'models', 'lodash', 'when',
           'text!views/destination-list.hbs',
           'text!views/tour-list.hbs',
-          'text!views/tour-details.hbs'
+          'text!views/tour-details.hbs',
+					'domReady!'
 ], function (Handlebars, $, models, _, when,
              destination_list_t, tour_list_t, tour_details_t) {
     "use strict";
@@ -68,49 +70,77 @@ require([ 'handlebars', 'jquery', 'models', 'lodash', 'when',
     var compiled_template;
     var rendered_template;
 
-    console.log(tour_details_t);
+		var register_nav_callbacks = function () {
 
-    var frontend_path = window.location.href.split('#')[1];
+				// $('.navigation').click(function () {
+				// 		console.log("registered navigation click callbacks");
+				// 		render();
+				// });
 
-    if (frontend_path === undefined || frontend_path == '') {
+		}
 
-        debugger;
+		function get_frontend_path() {
+				return window.location.href.split('#')[1];
+		}
 
-        get_destinations().then(function () {
-            var destinations_list_json = destinations.map(function (d) {
-                return d.make_list_json(); });
-            compiled_template = Handlebars.compile(destination_list_t);
-            rendered_template = compiled_template({
-                destinations: destinations_list_json
-            });
-            $('#destination-list').html(rendered_template);
-        });
-    } else {
+    var frontend_path = get_frontend_path();
 
-        debugger;
+		var render = function () {
 
-        compiled_template = Handlebars.compile(tour_list_t);
-        rendered_template = compiled_template({
-            tours: [{
-                id: 0,
-                name: "都江堰",
-                price: "$200",
-                img_url: 'img/dujiangyan.jpg'
-            }]
-        });
-        $('#tour-list').html(rendered_template);
+				if (frontend_path === undefined || frontend_path == '') {
 
-        compiled_template = Handlebars.compile(tour_details_t);
-        rendered_template = compiled_template({
-            name: "都江堰",
-            days: [ {description: "see the river"},
-                    {description: "see the mountain"}],
-            hometown: "成都",
-            introduction: "hey there.",
-            img_url_guide: 'img/kool-aid-man.jpg',
-            img_url_destination: 'img/dujiangyan.jpg'
-        });
-        $('#tour-detail').html(rendered_template);
-    }
+						get_destinations().then(function () {
+								var destinations_list_json = destinations.map(function (d) {
+										return d.make_list_json(); });
+								compiled_template = Handlebars.compile(destination_list_t);
+								rendered_template = compiled_template({
+										destinations: destinations_list_json
+								});
+								$('#destination-list').html(rendered_template);
+
+								$('#tour-list').html("");
+								$('#tour-detail').html("");
+								register_nav_callbacks();
+						});
+				} else {
+
+						compiled_template = Handlebars.compile(tour_list_t);
+						rendered_template = compiled_template({
+								tours: [{
+										id: 0,
+										name: "都江堰",
+										price: "$200",
+										img_url: 'img/dujiangyan.jpg'
+								}]
+						});
+						$('#tour-list').html(rendered_template);
+
+						compiled_template = Handlebars.compile(tour_details_t);
+						rendered_template = compiled_template({
+								name: "都江堰",
+								days: [ {description: "see the river"},
+												{description: "see the mountain"}],
+								hometown: "成都",
+								introduction: "hey there.",
+								img_url_guide: 'img/kool-aid-man.jpg',
+								img_url_destination: 'img/dujiangyan.jpg'
+						});
+						$('#tour-detail').html(rendered_template);
+
+						$('#destination-list').html("");
+						register_nav_callbacks();
+				}
+		}
+
+		render();
+
+		window.setInterval(function () {
+				var new_frontend_path = get_frontend_path();
+				if (frontend_path !== new_frontend_path) {
+						frontend_path = new_frontend_path;
+						render();
+				}
+		}, 100); // apparently used by Google
+// http://stackoverflow.com/questions/2161906/handle-url-anchor-change-event-in-js
 
 });
