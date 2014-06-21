7305e86c1bc8764548c7e4ec8172d22033e74799
Auvergne <auvergnerw@gmail.com>
2014-06-22 03:25:14 +0800

    debugging sample routes implementation

--- public/client/js/main.js
+++ public/client/js/main.js
@@ -9,7 +9,7 @@ require.config({
         'when': 'bower_components/when',
         'text': 'vendor/text',
 
-				'models': 'js/models'
+        'models': 'js/models'
     },
     packages: [
         {name: 'when', path: 'bower_components/when', main: 'when'}
@@ -26,82 +26,91 @@ require.config({
 });
 
 require([ 'handlebars', 'jquery', 'models', 'lodash', 'when',
-					'text!views/destination-list.hbs',
-					'text!views/tour-list.hbs',
-					'text!views/tour-details.hbs'
+          'text!views/destination-list.hbs',
+          'text!views/tour-list.hbs',
+          'text!views/tour-details.hbs'
 ], function (Handlebars, $, models, _, when,
-						 destination_list_t, tour_list_t, tour_details_t) {
+             destination_list_t, tour_list_t, tour_details_t) {
     "use strict";
 
-		var destinations = new models.Destinations();
+    var destinations = new models.Destinations();
 
     function get_destinations() {
         var p = when.defer();
-		    destinations.fetch({
+        destinations.fetch({
             success: function () { p.resolve(); },
             error: function () { p.reject(); }
-		    });
+        });
         return p.promise;
     }
 
 
-		Handlebars.registerHelper('list', function(items, options) {
-				var out = "<ul>";
-				for(var i=0, l=items.length; i<l; i++) {
-						_.extend(items[i], {num: i + 1});
-						out = out + "<li>" + options.fn(items[i]) + "</li>";
-				}
-				return out + "</ul>";
-		});
+    Handlebars.registerHelper('list', function(items, options) {
+        var out = "<ul>";
+        for(var i=0, l=items.length; i<l; i++) {
+            _.extend(items[i], {num: i + 1});
+            out = out + "<li>" + options.fn(items[i]) + "</li>";
+        }
+        return out + "</ul>";
+    });
 
-		Handlebars.registerHelper('tourlist', function(items, options) {
-				var out = "";
-				for(var i=0, l=items.length; i<l; i++) {
-						out = [out,
-									 '<div class="tour-list-item">',
-									 options.fn(items[i]),
-									 '</div>'].join('');
-				}
-				return out;
-		});
+    Handlebars.registerHelper('tourlist', function(items, options) {
+        var out = "";
+        for(var i=0, l=items.length; i<l; i++) {
+            out = [out,
+                   '<div class="tour-list-item">',
+                   options.fn(items[i]),
+                   '</div>'].join('');
+        }
+        return out;
+    });
 
     var compiled_template;
     var rendered_template;
 
     console.log(tour_details_t);
 
-    get_destinations().then(function () {
-        var destinations_list_json = destinations.map(function (d) {
-            return d.make_list_json(); });
-		    compiled_template = Handlebars.compile(destination_list_t);
-		    rendered_template = compiled_template({
-				    destinations: destinations_list_json
-		    });
-        $('#destination-list').html(rendered_template);
-    });
+    var frontend_path = window.location.href.split('#')[1];
 
-		compiled_template = Handlebars.compile(tour_list_t);
-		rendered_template = compiled_template({
-				tours: [{
-            id: 0,
+    if (frontend_path === undefined || frontend_path == '') {
+
+        debugger;
+
+        get_destinations().then(function () {
+            var destinations_list_json = destinations.map(function (d) {
+                return d.make_list_json(); });
+            compiled_template = Handlebars.compile(destination_list_t);
+            rendered_template = compiled_template({
+                destinations: destinations_list_json
+            });
+            $('#destination-list').html(rendered_template);
+        });
+    } else {
+
+        debugger;
+
+        compiled_template = Handlebars.compile(tour_list_t);
+        rendered_template = compiled_template({
+            tours: [{
+                id: 0,
+                name: "都江堰",
+                price: "$200",
+                img_url: 'img/dujiangyan.jpg'
+            }]
+        });
+        $('#tour-list').html(rendered_template);
+
+        compiled_template = Handlebars.compile(tour_details_t);
+        rendered_template = compiled_template({
             name: "都江堰",
-            price: "$200",
-						img_url: 'img/dujiangyan.jpg'
-        }]
-		});
-    $('#tour-list').html(rendered_template);
-
-		compiled_template = Handlebars.compile(tour_details_t);
-		rendered_template = compiled_template({
-				name: "都江堰",
-				days: [ {description: "see the river"},
-								{description: "see the mountain"}],
-				hometown: "成都",
-				introduction: "hey there.",
-				img_url_guide: 'img/kool-aid-man.jpg',
-				img_url_destination: 'img/dujiangyan.jpg'
-		});
-    $('#tour-detail').html(rendered_template);
-
+            days: [ {description: "see the river"},
+                    {description: "see the mountain"}],
+            hometown: "成都",
+            introduction: "hey there.",
+            img_url_guide: 'img/kool-aid-man.jpg',
+            img_url_destination: 'img/dujiangyan.jpg'
+        });
+        $('#tour-detail').html(rendered_template);
+    }
 
 });
