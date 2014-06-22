3504c230d049d8da40664d04fe2d8276d5c1665c
fahchen <dev.fah@gmail.com>
2014-06-21 23:58:41 -0300

    add image field to destinations

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621141844) do
+ActiveRecord::Schema.define(version: 20140622025805) do
 
   create_table "destinations", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "image"
   end
 
   create_table "tours", force: true do |t|
